bd148f5c0609f259e1ae806164bbeb241cb1a0d6
Martin Robinson <martinjrobins@gmail.com>
2016-10-30 07:11:07 +0000

    added fp fg and fP functions

--- soft-sphere-pde.cpp
+++ soft-sphere-pde.cpp
@@ -1,12 +1,24 @@
+#include <math.h>
+
+
 
 
 int main(void) {
     ABORIA_VARIABLE(density1p,double,"density")
     ABORIA_VARIABLE(density2p,double,"two particle density")
-    ABORIA_VARIABLE(constant,double,"constant for basis functions")
     ABORIA_VARIABLE(g_function,double,"g function")
 
-    typedef Particles<std::tuple<density1p,density2p,g_function,constant>,3> ParticlesType;
+    ABORIA_VARIABLE(density1p_weights,double,"density weights")
+    ABORIA_VARIABLE(density2p_weights,double,"two particle density weights")
+    ABORIA_VARIABLE(g_function_weights,double,"g function weights")
+
+    ABORIA_VARIABLE(density1p_weights0,double,"n-1 density weights")
+    ABORIA_VARIABLE(density2p_weights0,double,"n-1 two particle density weights")
+    ABORIA_VARIABLE(g_function_weights0,double,"n-1 g function weights")
+
+    ABORIA_VARIABLE(constant,double,"constant for basis functions")
+
+    typedef Particles<std::tuple<density1p,density2p,g_function,density1p_weights,density2p_weights,g_function_weights,,density1p_weights,density2p_weights,g_function_weights,constant>,3> ParticlesType;
     typedef position_d<3> position;
     ParticlesType knots;
 
@@ -14,16 +26,25 @@ int main(void) {
     const double L = 1.0;
     const double3 low(0,0,0);
     const double3 high(L,L,L);
-    const int max_iter = 100;
-    const int restart = 100;
+    const int max_iter_gmres = 100;
+    const int max_iter_newton = 100;
+    const int restart_gmres = 100;
     double2 periodic(false);
+    const double epsilon = 0.05;
+    const double alpha = 30.0;
+    const double theta = 0.2;
+    const double Tf = 0.02;
+    const double dt_aim = 0.001;
+    const int timesteps = Tf/dt;
+    const double dt = Tf/timesteps;
+    const int N = 16;
     
     const int nx = 7;
     const int ntheta = 5;
     const int nr = 5;
+    const int n = nx*ntheta*nr;
     const double deltar = 0.1;
     const double factorr = 1.01;
-    constexpr int N = (nx+1)*(nx+1);
     const double deltax = std::sqrt(1.0)/nx;
     const double deltatheta = 2*pi/nx;
     typename ParticlesType::value_type p;
@@ -37,17 +58,28 @@ int main(void) {
                 get<position>(p) = double3(x/std::sqrt(2.0),x/std::sqrt(2.0)+r*std::cos(theta),r*std::sin(theta));
                 if ((p<low).any() || (p>=high).any()) continue;
                 get<constant>(p) = 1.0/std::pow(c,2);
+                get<density1p>(p) = 0.5*(tanh(alpha*(x-theta))+tanh(alpha*(1-theta-x)));
                 knots.push_back(p);
             }
         }
     }
     std::cout << "added "<<knots.size()<<" knots" << std::endl;
+    std::cout << "expected "<<n<<" knots" << std::endl;
     knots.init_neighbour_search(low,high,std::pow(c,2),bool3(true,true,true));
 
     Symbol<position> r;
     Symbol<density1p> p;
     Symbol<density2p> P;
     Symbol<g_function> g;
+
+    Symbol<density1p_weights> wp;
+    Symbol<density2p_weights> wP;
+    Symbol<g_function_weights> wg;
+
+    Symbol<density1p_weights0> wp0;
+    Symbol<density2p_weights0> wP0;
+    Symbol<g_function_weights0> wg0;
+
     Symbol<constant> c;
     Label<0,ParticlesType> a(knots);
     Label<1,ParticlesType> b(knots);
@@ -120,28 +152,57 @@ int main(void) {
     auto K16 = deep_copy(exp(-c[b]*(pow(r[b][0]-r[a][1],2)+pow(r[b][1]-r[a][2],2))));
     auto K17 = deep_copy(exp(-c[b]*(pow(r[b][0]-r[a][2],2))));
 
+    auto fp = deep_copy((N-1)*sum(b,true,K1*wP[b]) + sum(b,true,K2*wp[b]));
+    auto fP = deep_copy((N-2)*(
+                (sum(b,true,K3*wP[b])*(sum(b,true,K6*wg[b]) 
+                                      + sum(b,true,K7*wg[b]) 
+                                      - sum(b,true,K8*wp[b])*sum(b,true,K9*wg[b])/sum(b,true,K5*wp[b])
+                                      - sum(b,true,K11*wp[b])*sum(b,true,K10*wg[b])/sum(b,true,K4*wp[b]))
+                + sum(b,true,K11*wP[b])*sum(b,true,K10*wg[b])
+                + sum(b,true,K12*wP[b])*sum(b,true,K9*wg[b]))/(sum(b,true,K4*wp[b])*sum(b,true,K5*wp[b]))
+                + sum(b,true,K13*wP[b])));
+    auto fg = deep_copy(sum(b,true,K15*wP[b])*sum(b,true,K16*wP[b])/sum(b,true,K17*wp[b]) - sum(b,true,K14*wg[b]));
 
 
-    Eigen::Map<Eigen::Matrix<double,n,1> > s_vect(get<scalar>(particles).data());
+    auto F1 = 
+    auto J = create_block_eigen_operator<3,3>(A,B,
+                                                     C,Zero);
+    typedef Eigen::Matrix<double,n,1> vector_type;
 
-
-    template<typename K, typename F>
-    solve(K kernel, F f) {
-        Eigen::Map<Eigen::Matrix<double,n,1> > source(get<temporary1>(knots).data());
-        Eigen::Map<Eigen::Matrix<double,n,1> > result(get<temporary2>(knots).data());
-        tmp1[a] = f;
+    template<typename Source, typename Result,typename Kernel>
+    solve(Kernel &kernel, vector_type &result, vector_type &source) {
         Eigen::GMRES<decltype(W), Eigen::DiagonalPreconditioner<double>> gmres;
-        gmres.set_restart(restart);
-        gmres.setMaxIterations(max_iter);
+        gmres.set_restart(restart_gmres);
+        gmres.setMaxIterations(max_iter_gmres);
         gmres.compute(create_eigen_operator(a,b,kernel));
         result = gmres.solve(source);
         std::cout << "GMRES:    #iterations: " << gmres.iterations() << ", estimated error: " << gmres.error() << std::endl;
-        return tmp2[a];
     }
 
+    vector_type w0; 
+    vector_type dw; 
+
+    //solve for w0 weights from initial condition
+    //
+    //solve for p weights
+    solve<density1p,density1p_weights>(K4);
+    
+    //estimate P and g
+    P[a] = sum(b,true,K4*wp)*sum(b,true,K5*wp);
+    solve<density2p,density2p_weights>(K3);
+
+    g[a] = sum(b,true,K15*wP)*sum(b,true,K16*wP)/sum(b,true,K17*wP);
+    solve<g_function,g_function_weights>(K14);
+
+    wp0[a] = wp[a];
+    wP0[a] = wP[a];
+    wg0[a] = wg[a];
+
     for (int i=0; i < timesteps; i++) {
-        for (int i=0; i < max_iter; i++) {
+        for (int i=0; i < max_iter_newton; i++) {
             //solve for J(x_n) (x_{n+1} - x_n) = -F(x_n)
+            solve(
+            
             //terminate if ||x_{n+1}-x_n|| < tol
         }
     }
